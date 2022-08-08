13c0339998b53a3da8b257e9423c56f7ff38378d
Lauren Kleinert <lauren.o.kleinert@gmail.com>
2022-08-07 17:29:28 -0700

    Added react-bootstrap and bootstrap dev dep. to package.json, built basic button for LandingPage.js

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,5 +1,12 @@
 const LandingPage = () => {
-  return <h1>Our App!</h1>;
+  return (
+    <>
+      <h1>Our App!</h1>
+      <button type="button" class="btn btn-primary">
+        Sign Up
+      </button>
+    </>
+  );
 };
 
 export default LandingPage;
