8f43e04cf7a2b5bb66b73320eb66c1ade4e999c5
Lauren Kleinert <lauren.o.kleinert@gmail.com>
2022-08-07 21:24:09 -0700

    Mock-up of landing page layout

--- src/App.js
+++ src/App.js
@@ -3,25 +3,27 @@ import "./App.css";
 import ProfilePage from "./pages/ProfilePage";
 import SearchPage from "./pages/SearchPage";
 import LandingPage from "./pages/LandingPage";
+//import bootstrap css?
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <NavLink to="/">Home</NavLink>
-      <br />
-      <NavLink end to="/users">
-        Search
-      </NavLink>
-      <br />
-      <NavLink to="/users/:id">User Profile</NavLink>
-      <br />
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/users" element={<SearchPage />} />
-        <Route path="/users/:id" element={<ProfilePage />} />
-        <Route path="*" element={"404 Error: Page Not Found"} />
-      </Routes>
-    </div>
+    // <div className="App">
+    //   <NavLink to="/">Home</NavLink>
+    //   <br />
+    //   <NavLink end to="/users">
+    //     Search
+    //   </NavLink>
+    //   <br />
+    //   <NavLink to="/users/:id">User Profile</NavLink>
+    //   <br />
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/users" element={<SearchPage />} />
+      <Route path="/users/:id" element={<ProfilePage />} />
+      <Route path="*" element={"404 Error: Page Not Found"} />
+    </Routes>
+    // </div>
   );
 }
 
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,11 +1,44 @@
+import "./LandingPage.css";
+import { Container, Row, Col, Button, Form } from "react-bootstrap";
+//figure out spacing in rows and columns
+
 const LandingPage = () => {
   return (
-    <>
-      <h1>Our App!</h1>
-      <button type="button" class="btn btn-primary">
-        Sign Up
-      </button>
-    </>
+    <Container fluid>
+      <Row className="top-bar m-5 mb-6">
+        <Col xs={7}>
+          <h1>CropSwap🌱</h1>
+        </Col>
+        <Col>
+          <Button variant="warning" type="submit">
+            Sign Up
+          </Button>
+        </Col>
+        <Col>
+          <Button>Log In</Button>
+        </Col>
+      </Row>
+      <Row className="landing-pic">
+        <Col xs={12} className="banner">
+          <h1>Find and Trade Fresh Local Produce</h1>
+        </Col>
+      </Row>
+      <Form>
+        <Row className="zipcode">
+          <Col xs={{ span: 4, offset: 4 }}>
+            <Form.Group controlId="formZipcode">
+              <Form.Label></Form.Label>
+              <Form.Control type="zipcode" placeholder="Enter your zipcode" />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Button variant="secondary" type="submit" className="submit">
+              Search
+            </Button>
+          </Col>
+        </Row>
+      </Form>
+    </Container>
   );
 };
 
