dd3cf1188371f23a5040e4d00778b6c35c8e73cf
Morgan Buntin <mmbuntin@gmail.com>
2022-08-10 13:23:27 -0400

    working on profilepage layout

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,15 +1,35 @@
-import { Container, Col, Row, Button } from "react-bootstrap";
+import axios from "axios";
+import { Container, Col, Row, Button, Card } from "react-bootstrap";
+import { useParams } from "react-router-dom";
 import "./ProfilePage.css";
+import { useEffect, useState } from "react";
 
 const ProfilePage = ({ authUser, setAuth }) => {
+  const [user, setUser] = useState("");
+
   const logoutUser = () => {
     localStorage.clear();
     setAuth(false, "");
   };
 
+  const { id } = useParams();
+
+  const getUser = () => {
+    axios
+      .get(`${process.env.REACT_APP_BACKEND_URL}/users/${id}`)
+      .then((result) => {
+        const user = result.data;
+        setUser(user);
+      });
+  };
+
+  useEffect(() => {
+    getUser();
+  });
+
   return (
     <Container fluid>
-      <Row className="top-bar m-5 mb-6">
+      <Row className="top-bar m-5 mb-4">
         <Col xs={7}>
           <h1>CropSwap🌱</h1>
         </Col>
@@ -37,9 +57,26 @@ const ProfilePage = ({ authUser, setAuth }) => {
           </Button>
         </Col>
       </Row>
-      <Row className="user-information">
-        <Col xs={12} className="banner">
-          <h1>User Info</h1>
+      <Row className="border border-success border-2 m-4 align-items-center">
+        <Col>
+          <h1>{user.firstName}'s Garden</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="border border-warning mx-4">
+          <h2>Bio</h2>
+          <p>
+            {!user.bio ? "Fill me out!" : user.bio}{" "}
+            {authUser === id ? <Button>Edit</Button> : ""}
+          </p>
+          <Card>
+            <Card.Body>
+              <Card.Title>Crops</Card.Title>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col className="border border-danger mx-4">
+          <h2>Test2</h2>
         </Col>
       </Row>
     </Container>
